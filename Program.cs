b30651ac375b45972ed4b76c25de7779d3ca69fd
null <null>
2023-09-20 12:27:27 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Arreglos;
+using System;
+
+namespace ArrelosUnidimensionlesG2_2024_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arreglo.Asignar();
+            Arreglo.Imprimir();
+            Arreglo.Invertir();
+            Console.WriteLine("\nOrden inverso");
+            Arreglo.Imprimir();
+            Console.ReadLine();
+        }
+    }
+}
